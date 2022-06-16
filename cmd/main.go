95edf3b0a994c38d73795eb27207e6a459302e58
Artem <garribonda@yandex.ru>
2022-06-16 14:05:31 +0300

    Распарсил шаблон в handlers.go, чтение переменных окружения

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/ArtemBonda/news/internal/handlers"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.IndexHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Fatalln(http.ListenAndServe(":"+port, mux))
+}
